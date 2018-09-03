84fb16ccef48cb8218bb377a971ab01bc1acb193
Pachito Marco Calabrese <marco@Pachitos-MacBook-Pro.local>
2018-09-03 10:35:20 +0200

    progress, connection to WebUSB

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-let devices = navigator.usb.getDevices()
+// 04f9:209d (vendorid:productId)
+let device;
 
-devices.then(devices => {
-  devices.map(device => {
-    console.log(device.productName);
-    console.log(device.manufacturerName);
-  });
-}).catch(e => console.log(e))
+const filters = [
+  {
+    vendorId: 0x04f9
+  }
+];
+
+
+function connect() {
+  let devices = navigator.usb.requestDevice(
+    { filters: filters }
+  )
+
+  devices
+  .then(selectedDevice => {
+      console.log(selectedDevice.productName);
+      console.log(selectedDevice.manufacturerName);
+      device = selectedDevice;
+      return device.open(); // Begin a session.
+  })
+  .catch(e => console.log(e))
+}
